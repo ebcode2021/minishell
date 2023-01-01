cb4f4bc1f1492db28171e02e1433575c848f24f0
jinholee <jinholee@student.42Seoul.kr>
2023-01-01 21:07:11 +0900

    feature::minor update to builtin_pwd. need to use ft_putstr_fd instead of printf

--- src/builtins/pwd.c
+++ src/builtins/pwd.c
@@ -6,13 +6,16 @@
 /*   By: jinholee <jinholee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/16 17:32:08 by eunson            #+#    #+#             */
-/*   Updated: 2022/12/20 09:06:16 by jinholee         ###   ########.fr       */
+/*   Updated: 2023/01/01 21:06:00 by jinholee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	builtin_pwd(char *directory)
+void	builtin_pwd(t_exec_block block)
 {
-	printf("%s\n", directory);
+	int	fd;
+
+	//set redirections
+	ft_putstr_fd(sys.pwd, fd);
 }
