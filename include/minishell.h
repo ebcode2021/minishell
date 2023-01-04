9e03e7fbce051f92a1148d240aa5b92d4f107750
jinholee <jinholee@student.42Seoul.kr>
2023-01-04 18:42:45 +0900

    chore::merge conflict

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: jinholee <jinholee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/12 12:42:34 by jinholee          #+#    #+#             */
-/*   Updated: 2023/01/04 18:40:02 by jinholee         ###   ########.fr       */
+/*   Updated: 2023/01/04 18:42:27 by jinholee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -101,11 +101,8 @@ void			builtin_cd(t_exec_block *block);
 void			builtin_pwd(void);
 void			builtin_env(void);
 void			builtin_echo(t_exec_block *execs);
-<<<<<<< HEAD
 void			builtin_unset(t_exec_block *exec);
 void			builtin_export(t_exec_block *exec);
-=======
 void			builtin_exit(t_exec_block *execs);
->>>>>>> feature/exit
 
 #endif
