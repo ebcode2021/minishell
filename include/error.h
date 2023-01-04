d55493a081efa0549640f792985f24d43a706972
ebcode2021 <ebcode2021@gmail.com>
2023-01-04 17:42:27 +0900

    add::error msg

--- include/error.h
+++ include/error.h
@@ -6,7 +6,7 @@
 /*   By: eunson <eunson@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/04 17:19:10 by eunson            #+#    #+#             */
-/*   Updated: 2023/01/04 17:26:37 by eunson           ###   ########.fr       */
+/*   Updated: 2023/01/04 17:42:08 by eunson           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,7 @@
 
 #define COMMAND_NOT_FOUND ": command not found"
 #define NOT_A_VALID_IDENTIFIER ": not a valid identifier"
+#define NUMERIC_ARGUMENT ": numeric argument required"
+#define TOO_MANY_ARG ": too many arguments"
 
 #endif
