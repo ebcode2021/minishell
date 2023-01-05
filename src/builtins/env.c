a48edcb2bad85cbbb3a546fc9850b7df24c8f492
jinholee <jinholee@student.42Seoul.kr>
2023-01-05 13:20:27 +0900

    feature::0 handling added

--- src/builtins/env.c
+++ src/builtins/env.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   env.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: eunson <eunson@student.42.fr>              +#+  +:+       +#+        */
+/*   By: jinholee <jinholee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/16 17:32:02 by eunson            #+#    #+#             */
-/*   Updated: 2023/01/05 13:14:00 by eunson           ###   ########.fr       */
+/*   Updated: 2023/01/05 13:20:04 by jinholee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void	builtin_env(void)
 	head = g_sys.env_lst;
 	while (head)
 	{
-		if (head->value)
+		if (head->value && head->variable_name != '?')
 			ft_putendl_fd(head->copy, STDOUT_FILENO);
 		head = head->next;
 	}
